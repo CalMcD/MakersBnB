55582a84ef845fcb7b6ae22aea019ccf253d03a6
Callum McDoiugall <cal-mcd@outlook.com>
2020-01-23 14:53:50 +0000

    2 failing tests

--- spec/unit/add_listing_spec.rb
+++ spec/unit/add_listing_spec.rb
@@ -1,15 +1,14 @@
-describe Viewlist do
+describe Listings do
 
   it 'should add the new listing to the database' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO listings (place_name, ppn, guests, description) VALUES('A really nice house', '200', '5', 'This house is nice')")
+    connection.exec("INSERT INTO listings (place_name, ppn, guests, description) VALUES('A really nice house', '200', '5', 'This house is nice');")
 
-    listings = Viewlist.all
+    listings = Listings.all
 
-    expect(listings).to include "a really nice house"
+    expect(listings).to include "A really nice house"
     expect(listings).to include "200"
     expect(listings).to include "5"
     expect(listings).to include "This house is nice"
   end
-
 end
--- spec/unit/viewing_listings_spec.rb
+++ spec/unit/viewing_listings_spec.rb
@@ -1,9 +1,19 @@
-# require 'listings'
-#
-# describe Viewlist do
-#   describe '.all' do
-#     it 'returns all listings' do
-#       connection = PG.connect(dbname: 'makersbnb_test')
-#       listing =
-#   end
-# end
+require 'listings'
+
+describe Listings do
+  describe '.all' do
+    it 'returns all listings' do
+      connection = PG.connect(dbname: 'makersbnb_test')
+      connection.exec("INSERT INTO listings VALUES('Nice Villa', '8', '10', 'Its nice');")
+
+      # listings = Listings.add("Nice Villa", "8", "10", "Its nice")
+
+      listings = Listings.all
+
+      expect(listings).to include('Nice Villa')
+      expect(listings).to include('8')
+      expect(listings).to include('10')
+      expect(listings).to include('Its nice')
+      end
+  end
+end
